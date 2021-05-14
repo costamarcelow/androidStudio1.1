8b8301a12c82970ca7243571fb6ba5b64395002e
costamarcelow <50969725+costamarcelow@users.noreply.github.com>
2021-05-14 19:43:01 -0300

    Corrigido erro no editText não estava com a referencia ID correta

--- app/src/main/java/com/example/senai/MainActivity.java
+++ app/src/main/java/com/example/senai/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickBtnMostrar(View v){
-        EditText editText = findViewById(R.id.linearLayout);
+        EditText editText = findViewById(R.id.et_texto);
         EditText editText2 = findViewById(R.id.et_texto2);
         Toast.makeText(MainActivity.this, editText.getText()+ " " + editText2.getText(), Toast.LENGTH_LONG).show();
     }
 
     public void onClickBtnLimpar (View v){
-        EditText editText = findViewById(R.id.linearLayout);
+        EditText editText = findViewById(R.id.et_texto);
         EditText editText2 = findViewById(R.id.et_texto2);
         editText.setText(" ");
         editText2.setText(" ");
